0315a0faf62cc7b440aa3b84cb748e3889317ce1
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-05-28 06:34:15 +0700

    refactor [welcome, navigation]: adjust the style

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,11 +1,11 @@
-<header class="sticky top-0 z-10 bg-white shadow">
+<header class="sticky top-0 z-10 bg-black shadow text-gray-300 text-cabin">
   <div class="max-w-7xl mx-auto">
     <nav class="Navbar md:flex justify-start gap-20 items-center py-6 mx-4">
       <ul class="flex items-center justify-between">
         <li>
           <a id="brand" href="/">
-            <h1 class="text-xl font-semibold  transition-all duration-500">
-              <span class="text-[#00C0E6]">Sh</span>hh.
+            <h1 class="text-xl font-semibold transition-all duration-500 text-londrina shhh">
+              <span class="">Sh</span>hh.
             </h1>
           </a>
         </li>
@@ -18,10 +18,10 @@
         <ul class="md:flex gap-5 transition-all duration-500">
           @if (auth()->user())
             <li class="my-5 md:my-0">
-              <a href="{{ route('dashboard') }}" class="hover:text-[#00C0E6] transition-all duration-500">Dashboard</a>
+              <a href="{{ route('dashboard') }}" class="hover:text-white transition-all duration-500">Dashboard</a>
             </li>
             <li class="my-5 md:my-0">
-              <a href="{{ route('messages') }}" class="hover:text-[#00C0E6] transition-all duration-500">Messages</a>
+              <a href="{{ route('messages') }}" class="hover:text-white transition-all duration-500">Messages</a>
             </li>
           @else
             <li class="text-gray-400 my-5 md:my-0">
@@ -33,13 +33,13 @@
           <ul class="md:flex gap-5">
             @if (Route::has('login'))
               <li class="my-5 md:my-0">
-                <a href="{{ route('login') }}" class="hover:text-[#00C0E6] transition-all duration-500">Login</a>
+                <a href="{{ route('login') }}" class="hover:text-white transition-all duration-500">Login</a>
               </li>
             @endif
 
             @if (Route::has('register'))
               <li class="my-5 md:my-0">
-                <a href="{{ route('register') }}" class="hover:text-[#00C0E6] transition-all duration-500">Register</a>
+                <a href="{{ route('register') }}" class="hover:text-white transition-all duration-500">Register</a>
               </li>
             @endif
           </ul>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,20 @@
 <x-app-layout>
-   @section('content')
+    <div
+        id="welcome"
+        class="welcome-canvas flex min-h-screen justify-center items-center bg-black"
+    >
+        <div class="flex flex-col gap-5 text-center mb-32 max-w-5xl">
+            <h1 data-aos="zoom-in" data-aos-duration="1000" class="shhh text-londrina text-6xl text-white font-semibold transition-all duration-500">
+                shhh.
+            </h1>
+            <p data-aos="zoom-out" data-aos-duration="1500" class="text-londrina text-7xl text-white font-semibold transition-all duration-500">
+                @php $quotes = [ "Spill the tea...
+                anonymously.", "Say it. Secretly.", "Speak your mind. Unmasked.", "Unleash your thoughts. Incognito." ]; $randomQuote =
+                $quotes[array_rand($quotes)]; @endphp
 
-   <div class="max-w-7xl mx-auto flex h-screen justify-center items-center">
-    helo
-   </div>
+                {{ $randomQuote }}
+
+            </p>
+        </div>
+    </div>
 </x-app-layout>
